85aad95d1aa74bce7a4e6e2ca78d41a2c0de5161
Charles Ewert <cewert@gmail.com>
2019-02-28 12:38:55 -0500

    Updates

---
+++ precache-manifest.d9ace13239a29bca0ccd0df125692563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "7ba336966a9dad25c73d",
+    "url": "/book-finder/static/js/main.7ba33696.chunk.js"
+  },
+  {
+    "revision": "8593d4d2833458843ca0",
+    "url": "/book-finder/static/js/2.8593d4d2.chunk.js"
+  },
+  {
+    "revision": "7ba336966a9dad25c73d",
+    "url": "/book-finder/static/css/main.0eec4d27.chunk.css"
+  },
+  {
+    "revision": "f33057a22a14bc3d912df66f70141e5f",
+    "url": "/book-finder/index.html"
+  }
+];
