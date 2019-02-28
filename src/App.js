27461ed0b2dbf47ab705bf9272aeb100c76200f7
Charles Ewert <cewert@gmail.com>
2019-02-28 12:31:39 -0500

    Added API call as well as made use of other components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
+import BookResults from "./components/BookResults/BookResults";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      books: []
+    };
+
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.handleQuerySubmit = this.handleQuerySubmit.bind(this);
+  }
+
+  handleQueryChange(query) {
+    this.setState({ searchQuery: query });
+  }
+
+  handleQuerySubmit() {
+    // TODO add &startIndex= to query string to support paginating results
+    let baseURL =
+      "https://www.googleapis.com/books/v1/volumes?maxResults=40&q=";
+
+    // get volume info from API
+    fetch(baseURL + this.state.searchQuery)
+      .then(response => {
+        response.json().then(data => {
+          // save volume info to state
+          this.setState({ books: data.items });
+          console.log(data.items);
+        });
+      })
+      .catch(function(err) {
+        console.log("Fetch Error", err);
+      });
+    console.log("searchQuery = " + this.state.searchQuery);
+  }
+
   render() {
     return (
       <div className="App">
-        <p> </p>
+        <h1>Book Finder</h1>
+        <SearchBar
+          searchQuery={this.state.searchQuery}
+          handleQueryChange={this.handleQueryChange}
+          handleQuerySubmit={this.handleQuerySubmit}
+        />
+        <BookResults books={this.state.books} />
       </div>
     );
   }
